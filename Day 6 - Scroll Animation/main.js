46b809450a273ba68253b3f8d21346e5f6389686
Omar Ashraf <omar_ashraf_93@hotmail.com>
2021-04-01 11:14:59 +0300

    Optimized js file & Css file as Brad Traversey Made it (His Method was way easier)

--- Day 6 - Scroll Animation/main.js
+++ Day 6 - Scroll Animation/main.js
@@ -2,23 +2,14 @@ let myContents = Array.from(document.querySelectorAll(".content"));
 
 window.onload = function () {
   window.scrollTo(0, 0);
-  myContents.forEach((content) => {
-    console.log(content.innerHTML, content.offsetTop - window.scrollY);
-    if (content.offsetTop - window.scrollY >= 630) {
-      content.style.display = "none";
-    }
-  });
 };
+
 window.onscroll = function () {
   myContents.forEach((content) => {
-    console.log(content.innerHTML, content.offsetTop - window.scrollY);
-    if (content.offsetTop - window.scrollY <= 630) {
-      content.style.display = "block";
-      content.classList.add("active");
-      content.classList.remove("active-reverse");
+    if (content.offsetTop - window.scrollY <= 600) {
+      content.classList.add("show");
     } else {
-      content.classList.remove("active");
-      content.classList.add("active-reverse");
+      content.classList.remove("show");
     }
   });
 };
