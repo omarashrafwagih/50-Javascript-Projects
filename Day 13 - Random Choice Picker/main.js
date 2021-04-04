55f7bebe2964d1210163fadaae12443290316584
Omar Ashraf <omar_ashraf_93@hotmail.com>
2021-04-04 15:41:54 +0300

    Added Random Choice Picker Project

---
+++ Day 13 - Random Choice Picker/main.js
@@ -0,0 +1,44 @@
+let textarea = document.querySelector("textarea");
+let text = "";
+let arrayText = [];
+let choicesDiv = document.querySelector(".choices");
+
+textarea.addEventListener("keyup", () => {
+  text = textarea.value;
+  arrayText = text.split(",");
+  choicesDiv.innerHTML = "";
+  createChoices();
+});
+
+function createChoices() {
+  arrayText.forEach((elem) => {
+    let choice = document.createElement("div");
+    choice.classList.add("choice");
+    choice.textContent = elem;
+    choicesDiv.appendChild(choice);
+  });
+}
+
+textarea.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    textarea.value = "";
+
+    let intervalCode = setInterval(() => {
+      let randomNum = null;
+      randomNum = Math.floor(Math.random() * choicesDiv.children.length);
+      for (let i = 0; i < choicesDiv.children.length; i++) {
+        choicesDiv.children[i].classList.remove("active");
+      }
+      choicesDiv.children[randomNum].classList.add("active");
+    }, 100);
+
+    setTimeout(() => {
+      clearInterval(intervalCode);
+    }, 3000);
+  }
+});
+
+document.querySelector(".refresh").onclick = function () {
+  textarea.value = "";
+  choicesDiv.innerHTML = "";
+};
